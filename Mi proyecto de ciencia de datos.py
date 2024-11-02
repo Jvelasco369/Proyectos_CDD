eb84a6a99fafc29daaac2050fbeca238165690dd
Jvelasco369 <yisaivelasco@gmail.com>
2024-11-01 20:34:50 -0600

    Add files via upload

---
+++ Mi proyecto de ciencia de datos.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Mi Proyecto de Ciencia de Datos
+
+
+# In[ ]:
+
+
+## Introducción
+
+Este cuaderno tiene como objetivo explorar las herramientas y bibliotecas utilizadas en ciencia de datos. A través de varios ejercicios, demostraremos cómo utilizar funciones básicas y realizar cálculos sencillos en Python.
+
+
+# In[ ]:
+
+
+## Lenguajes de Programación en Ciencia de Datos
+
+1. Python
+2. R
+3. SQL
+4. Julia
+5. Scala
+6. Java
+7. MATLAB
+
+
+# In[1]:
+
+
+## Bibliotecas de Ciencia de Datos
+
+1. **NumPy** - Para cálculos numéricos y manejo de arreglos.
+2. **Pandas** - Para análisis y manipulación de datos.
+3. **Matplotlib** - Para visualización de datos.
+4. **Seaborn** - Para visualización estadística de datos.
+5. **Scikit-Learn** - Para aprendizaje automático.
+6. **TensorFlow** - Para aprendizaje profundo.
+7. **Keras** - Para construir y entrenar modelos de redes neuronales.
+8. **PyTorch** - Para procesamiento y entrenamiento en aprendizaje profundo.
+9. **NLTK** - Para procesamiento de lenguaje natural.
+10. **Statsmodels** - Para análisis estadístico.
+
+
+# In[ ]:
+
+
+## Herramientas de Ciencia de Datos
+
+| Herramienta       | Descripción                                |
+|-------------------|--------------------------------------------|
+| Jupyter Notebook  | Entorno interactivo para el análisis de datos. |
+| RStudio           | Entorno de desarrollo integrado para R.    |
+| Tableau           | Herramienta de visualización de datos.     |
+| Power BI          | Herramienta de inteligencia empresarial.   |
+| Apache Spark      | Motor de análisis de grandes datos.        |
+
+
+# In[2]:
+
+
+## Ejemplos de expresiones aritméticas
+
+1. Suma: \(5 + 3 = 8\)
+2. Resta: \(10 - 7 = 3\)
+3. Multiplicación: \(4 \times 6 = 24\)
+4. División: \(20 \div 5 = 4\)
+
+
+# In[3]:
+
+
+# Multiplicación y suma de números
+a = 5
+b = 3
+multiplicacion = a * b
+suma = a + b
+multiplicacion, suma
+
+
+# In[4]:
+
+
+# Convertir minutos en horas
+minutos = 120
+horas = minutos / 60
+horas
+
+
+# In[5]:
+
+
+# Lista de Objetivos
+objetivos = ["Aprender Python", "Entender la Ciencia de Datos", "Aplicar modelos de ML", "Visualizar datos efectivamente"]
+for objetivo in objetivos:
+    print(objetivo)
+
+
+# In[6]:
+
+
+**Autor:** [J Velasco]
+
+
+# In[ ]:
+
+
+
+
